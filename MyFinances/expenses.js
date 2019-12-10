e27b711ed209232d3fc9cf6006923ba982690ded
Ruben Ferreira <rubenfferreira1905@gmail.com>
2019-12-10 00:13:05 +0000

    9/12 despesas

--- MyFinances/expenses.js
+++ MyFinances/expenses.js
@@ -1,9 +1,9 @@
 /* função que soma o nosso gasto atual*/
-let sum=0
+let sum = 0
 btn.addEventListener("click", function () {
     let inp = +document.querySelector("#food").value
-    let act=+document.querySelector("#actual").value
-    sum=act+inp
-   document.querySelector("#actual").value=sum 
-
+    let act = +document.querySelector("#actual").value
+    sum = act + inp
+    document.querySelector("#actual").value = sum
+    document.querySelector("#food").value = 0
 })
