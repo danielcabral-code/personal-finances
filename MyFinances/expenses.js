5d65aa6586e1e34074532ca312d17173e38c918e
Ruben Ferreira <rubenfferreira1905@gmail.com>
2019-12-11 17:56:18 +0000

    collapse buttons despesas 11/12

--- MyFinances/expenses.js
+++ MyFinances/expenses.js
@@ -2,8 +2,23 @@
 let sum = 0
 btn.addEventListener("click", function () {
     let inp = +document.querySelector("#food").value
-    let act = +document.querySelector("#actual").value
+    let act = +document.querySelector(".actual").value
     sum = act + inp
-    document.querySelector("#actual").value = sum
+    document.querySelector(".actual").value = sum
     document.querySelector("#food").value = 0
 })
+
+/*função para o botão collapse*/
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  })}
