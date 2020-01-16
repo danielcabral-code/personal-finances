3353fb66fb840a1400bb053b757640533a3a3cb8
Ruben Ferreira <rubenfferreira1905@gmail.com>
2020-01-16 14:34:39 +0000

    Revert "a"
    This reverts commit f985b3fdd7742e0b603cc3436f12ce4c82976768.

--- MyFinances/expenses-list.js
+++ MyFinances/expenses-list.js
@@ -18,22 +18,12 @@ function selectExpense() {
     for (let i = 0; i < expenses.length; i++) {
         let savedItem = expenses[i]
        console.log(savedItem);
-       
-        
-      
-
-       
-     
       }
       
       
    
       arr.forEach(function (savedItem) {
-            
-    
-    
-    
-           
+     
         let option = document.createElement("option");
         option.text = savedItem.date;
         list.add(option);
@@ -42,3 +32,49 @@ function selectExpense() {
 }
 
 selectExpense();
+
+list.onchange = function()
+{
+    let foundDate = false;
+    let savedItem;
+    let compareDate = document.querySelector("#selectDate").value;
+    for (let i = 0; i < expenses.length; i++)
+    {
+        savedItem = expenses[i];
+
+        if (savedItem.date === compareDate)
+        {
+            foundDate = true;
+            break;
+        }
+    }
+
+    if (foundDate)
+        LoadData(savedItem);
+}
+
+function LoadData(savedItem)
+{
+    document.getElementById("actualFood").value = savedItem.generalFood
+    document.querySelector("#actualAwayFood").value = savedItem.restaurant
+    document.querySelector("#actualCin").value = savedItem.movies
+    document.querySelector("#actualSport").value = savedItem.sports
+    document.querySelector("#actualGym").value = savedItem.gym
+    document.querySelector("#actualOut").value = savedItem.nightOut
+    document.querySelector("#actualTrip").value = savedItem.trip
+    document.querySelector("#actualElect").value = savedItem.electricity
+    document.querySelector("#actualWater").value = savedItem.water
+    document.querySelector("#actualIncome").value = savedItem.income
+    document.querySelector("#actualNet").value = savedItem.internet
+    document.querySelector("#actualOtherPay").value = savedItem.otherHousePay
+    document.querySelector("#actualPharm").value = savedItem.pharm
+    document.querySelector("#actualDoctor").value = savedItem.doctor
+    document.querySelector("#actualOtherHealth").value = savedItem.otherHealthPay
+    document.querySelector("#actualFuel").value = savedItem.fuel
+    document.querySelector("#actualMaintenance").value = savedItem.carMaintenance
+    document.querySelector("#actualInsurance").value = savedItem.carInsurance
+    document.querySelector("#actualCarFine").value = savedItem.carFine
+
+    document.getElementById("informations").style.display = "block";
+    document.getElementById("divData").style.display = "none";
+}
