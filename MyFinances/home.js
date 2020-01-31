1a3788c04dc74f40de66b107639d1fdf67c890a3
danielcabral-code <57807464+danielcabral-code@users.noreply.github.com>
2020-01-31 01:50:04 +0000

    31.1

--- MyFinances/home.js
+++ MyFinances/home.js
@@ -1,3 +1,4 @@
+// Variáveis globais
 let currencyContainer = document.getElementById("converted")
 let currency = document.getElementById("currency")
 let selectedCurrency
@@ -5,18 +6,15 @@ let euroConvert = 0
 let usdConvert = 0
 let gbpConvert = 0
 
-//função que vai buscar os valores da moeda à API e atribui a variáveis consoante a pesquisa do utiliozador
+// Função que vai buscar os valores da moeda à API e atribui a variáveis consoante a pesquisa do utilizador
 async function convert(value) {
     selectedCurrency = currency.options[currency.selectedIndex].text
 
     if (selectedCurrency === "EUR") {
-
         const response = await fetch(`https://api.exchangeratesapi.io/latest?&symbols=USD,GBP`)
         myJson = await response.json()
-
         usdConvert = Math.floor(myJson.rates.USD * 100) * value / 100
         gbpConvert = Math.floor(myJson.rates.GBP * 100) * value / 100
-
         currencyContainer.innerHTML = `<p>USD ${usdConvert}</p><p>GBP ${gbpConvert}</p>`
 
     } else if (selectedCurrency === "USD") {
@@ -24,43 +22,40 @@ async function convert(value) {
         myJson = await response.json()
         euroConvert = Math.floor(myJson.rates.EUR * 100) * value / 100
         gbpConvert = Math.floor(myJson.rates.GBP * 100) * value / 100
-
         currencyContainer.innerHTML = `<p>EUR ${euroConvert}</p><p>GBP ${gbpConvert}</p>`
+
     } else {
         const response = await fetch(`https://api.exchangeratesapi.io/latest?base=${selectedCurrency}&symbols=EUR,USD`)
         myJson = await response.json()
         euroConvert = Math.floor(myJson.rates.EUR * 100) * value / 100
         usdConvert = Math.floor(myJson.rates.USD * 100) * value / 100
-
         currencyContainer.innerHTML = `<p>EUR ${euroConvert}</p><p>USD ${usdConvert}</p>`
     }
 }
 
-//botão que vai executar a função de converter a moeda
+// Botão que vai executar a função de converter a moeda
 doConversion.addEventListener("click", function () {
-
     let valueToConvert = +document.getElementById("valueToConvert").value
     convert(valueToConvert);
 })
 
-//verificação se o localStorage onde é guardado o saldo do utilizador existe.Se existir é carregado senão é criado um novo com o valor de 0.
-//aplica cor vermnelha se estiver negativo e cor verde se estiver positivo ou 0
+// Verificação se o localStorage onde é guardado o saldo do utilizador existe; se existir é carregado senão é criado um novo com o valor de 0
+// Aplica cor vermnelha se estiver negativo e cor verde se estiver positivo ou 0
 let money = 0
 if (localStorage.getItem('money')) {
     money = JSON.parse(localStorage.getItem('money'))
     document.querySelector("#currentMoney").value = money
 
     if (money >= 0) {
-
-        document.querySelector("#currentMoney").style.color = "green"
+        document.querySelector(".moneyAndSymbol").style.color = "green"
     } else {
-        document.querySelector("#currentMoney").style.color = "red"
+        document.querySelector(".moneyAndSymbol").style.color = "red"
     }
 } else {
-    money
+    money;
 }
 
-//função que verifica se o utilizador está a tentar carregar no saldo um valor negativo 
+// Função que verifica se o utilizador está a tentar carregar no saldo um valor negativo 
 function checkNegativeValue() {
 
     let value = parseFloat(document.getElementById("addMoney").value);
@@ -70,7 +65,7 @@ function checkNegativeValue() {
     }
 }
 
-//Botão que irá acrescentar o saldo do utilizador. Verifica depois de acrescentar se o saldo está negativo ou positivo.
+// Botão que acrescenta o saldo do utilizador; verifica depois de acrescentar se o saldo está negativo ou positivo.
 btnAddMoney.addEventListener("click", function () {
 
     checkNegativeValue();
@@ -85,12 +80,9 @@ btnAddMoney.addEventListener("click", function () {
     localStorage.setItem("money", JSON.stringify(money))
 
     if (money >= 0) {
-
         document.querySelector("#currentMoney").style.color = "green"
     } else {
         document.querySelector("#currentMoney").style.color = "red"
     }
     document.querySelector("#addMoney").value = null
-
-
 })
